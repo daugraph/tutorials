b65972027bda6eebc00a54dc97c2ea5479d3c9f1
lijianmeng <lijianmeng@qiyi.com>
2021-02-20 20:52:39 +0800

    remove unused code

--- tensorflow/client-master-worker/client.py
+++ tensorflow/client-master-worker/client.py
@@ -2,11 +2,6 @@ import json
 import tensorflow.compat.v1 as tf
 tf.disable_v2_behavior()
 
-with open('clusterspec.json', 'r') as f:
-    clusterspec = json.load(f)
-
-cluster = tf.train.ClusterSpec(clusterspec)
-
 a = tf.constant(3)
 b = tf.constant(2)
 
@@ -17,5 +12,5 @@ with tf.device("/job:worker/task:1"):
     y = tf.multiply(a, b)
 
 # 给session显式指定target
-with tf.Session("grpc://10.67.8.141:20001", config=tf.ConfigProto(log_device_placement=True)) as sess:
+with tf.Session("grpc://localhost:20001", config=tf.ConfigProto(log_device_placement=True)) as sess:
     print(sess.run([x, y]))
